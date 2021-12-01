93070be503a5dc3319c530c06894fba55f0029ff
Alasdair <alasdairjbbennett@gmail.com>
2021-11-30 22:40:30 -0500

    Add chess board

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import './App.css';
+import { useEffect, useState } from "react";
+// import { Chessboard } from 'react-chessboard';
+// import ReactDOM from 'react-dom';
+
+import BasicBoard from './BasicBoard.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Blunder-Dodger
-        </p>
-      </header>
-    </div>
-  );
+    const [chessboardSize, setChessboardSize] = useState('BasicBoard');
+    // const [selectedBoard, setSelectedBoard] = useState('BasicBoard');
+
+    useEffect(() => {
+        function handleResize() {
+            const display = document.getElementsByClassName('container')[0];
+            setChessboardSize(display.offsetWidth - 20);
+        }
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+
+    return (
+        <div className="container">
+            <h1>Basic Board ex</h1>
+            <>
+                <h2>Basic Board</h2>
+                <BasicBoard boardWidth={chessboardSize} />
+                <br />
+            </>
+        </div>
+    );
 }
 
 export default App;
